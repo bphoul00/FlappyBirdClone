7c376a8b2f5f3b791838cd5beca7a58c5fa1d20e
BernardH <phavutha.h@gmail.com>
2019-02-16 19:28:27 -0500

    Start the project and add the asset

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public static BirdScript instance;
+
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private Animator anim;
+
+    private float fowardSpeed = 3f;
+
+    private float bounceSpeed = 4f;
+
+    private bool didFlag;
+
+    public bool isAlive;
+
+    private void Awake()
+    {
+        if(instance == null) {
+            instance = this;
+        }
+        isAlive = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (isAlive) {
+            Vector3 temp = transform.position;
+            temp.x += fowardSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            if (didFlag) {
+                didFlag = false;
+                myRigidBody.velocity = new Vector2(0, bounceSpeed);
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    public void FlapTheBird() => didFlag = true;
+}
